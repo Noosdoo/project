de1e0a788e9774d50866d3910e2aae667929e02a
[mappyex-user] <[mappyex@gmail.com]>
2025-10-21 18:33:19 +0900

    アウトプットふぁいる

--- handle.py
+++ handle.py
@@ -1,1 +1,9 @@
-aaaaaaaaaaaaaaarjiejriejroqjeorjrwwsss
+
+#出力ファイルの読み込み
+try:
+    f = open('output.txt','r', encoding='UTF-8')
+    date=f.read()
+except:
+    print("出力ファイルが読み込めませんでした。")
+    
+print("それは"+date+"ですか？")
