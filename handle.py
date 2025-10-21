fe5ba4577042048e99b088a2bc7575e8368c46e9
[mappyex-user] <[mappyex@gmail.com]>
2025-10-21 18:41:06 +0900

    aa

--- handle.py
+++ handle.py
@@ -7,7 +7,7 @@ except:
     print("出力ファイルが読み込めませんでした。")
     
 #選択肢入力    
-input=input("それは"+date+"ですか？")
+input=input("それは"+date+"ですか？\n")
 
 # ファイルを開く（存在しなければ新規作成）
 with open("input.txt", "w", encoding="utf-8") as f:
