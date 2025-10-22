640a88b832408c3e7f04b1f0fa6fd9eb77ab6f28
[mappyex-user] <[mappyex@gmail.com]>
2025-10-22 14:48:48 +0900

    aaaa

--- handle.py
+++ handle.py
@@ -1,14 +1,17 @@
-
-#出力ファイルの読み込み
-try:
-    f = open('output.txt','r', encoding='UTF-8')
-    date=f.read()
-except:
-    print("出力ファイルが読み込めませんでした。")
+while True:
+    #出力ファイルの読み込み
+    try:
+        f = open('output.txt','r', encoding='UTF-8')
+        date=f.read()
+        
+    except:
+        print("出力ファイルが読み込めませんでした。")
+        
+    #選択肢入力    
+    input_str=input("それは"+date+"ですか？\n")
     
-#選択肢入力    
-input=input("それは"+date+"ですか？\n")
-
-# ファイルを開く（存在しなければ新規作成）
-with open("input.txt", "w", encoding="utf-8") as f:
-    f.write(input)
+    if input_str=="end":
+        break
+    # ファイルを開く（存在しなければ新規作成）
+    with open("input.txt", "w", encoding="utf-8") as f:
+        f.write(input_str)
