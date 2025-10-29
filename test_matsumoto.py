217f5dbc2b00d3b83d1ec3edb79555854275de6f
Noosdoo <satoshimatsumoto74@gmail.com>
2025-10-29 15:18:24 +0900

    s

--- test_matsumoto.py
+++ test_matsumoto.py
@@ -627,7 +627,7 @@ def run_step(step="collect", **kwargs):
 
 if __name__ == "__main__":
     selected_categories = choose_categories()
-    run_step("collect", categories=selected_categories, cmlimit=20, depth=0, sleep=0.0001)
+    run_step("collect", categories=selected_categories, cmlimit=20, depth=1, sleep=0.0001)
 
     run_step("build", limit=200, sleep=0.0001)
     run_step("play", max_questions=25)
