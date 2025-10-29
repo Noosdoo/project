f3c61702c2c4018343c3613628c5fa1825befaad
Noosdoo <satoshimatsumoto74@gmail.com>
2025-10-29 16:12:00 +0900

    s

--- test_matsumoto.py
+++ test_matsumoto.py
@@ -646,7 +646,7 @@ def run_step(step="collect", **kwargs):
 if __name__ == "__main__":
     selected_categories = choose_categories()
     # データ収集
-    run_step("collect", cmlimit=20, depth=1, sleep=0.0001)
+    run_step("collect", categories=selected_categories, cmlimit=20, depth=1, sleep=0.0001)
     # データセットを構築
     run_step("build", limit=200, sleep=0.0001)
     # アキネーターをプレイ
