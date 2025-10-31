7383b8f53e239bd0e2b342b7df473afe71ff660c
dahuicunshang87 <dahuicunshang87@gmail.com>
2025-10-31 15:04:07 +0900

    変更

--- test_matsumoto3-2.py
+++ test_matsumoto3-2.py
@@ -476,9 +476,9 @@ def generate_question_map(dataset, selected_categories=None):
     common_questions_def = [
         ("gender_male", "男性ですか？", "common"), ("gender_female", "女性ですか？", "common"),
         ("alive_text", "現在もご存命ですか？", "common"),
-        ("actor_wikidata", "本業は俳優ですか？", "occupation"),
-        ("singer_wikidata", "本業は歌手ですか？", "occupation"),
-        ("politician_wikidata", "本業は政治家ですか？", "occupation"),
+        ("actor_wikidata", "職業は俳優ですか？", "occupation"),
+        ("singer_wikidata", "職業は歌手ですか？", "occupation"),
+        ("politician_wikidata", "職業は政治家ですか？", "occupation"),
         ("age_20s", "現在、20代ですか？", "common"), ("age_30s", "現在、30代ですか？", "common"),
         ("age_40s", "現在、40代ですか？", "common"), ("age_50s", "現在、50代ですか？", "common"),
         ("born_1980s", "1980年代生まれですか？", "common"), ("born_1990s", "1990年代生まれですか？", "common"),
