40dd8ce4e912946ae8393dd8d7d4ea48ed5fde00
Noosdoo <satoshimatsumoto74@gmail.com>
2025-11-11 13:41:37 +0900

    d

--- matsumoto/test_matsumoto_back_ver.py
+++ matsumoto/test_matsumoto_back_ver.py
@@ -605,7 +605,7 @@ def load_people_list(people_list_path=PEOPLE_LIST_FILE):
 # (summaryの取得状況をログに出す)
 # -----------------------
 def build_dataset_parallel(people_list_path=PEOPLE_LIST_FILE, dataset_path=DATASET_FILE,
-                           limit=None, max_workers=20, sleep=0.1):
+                           limit=None, max_workers=50, sleep=0.1):
     
     # 人物リスト読み込み
     people = load_people_list(people_list_path)
