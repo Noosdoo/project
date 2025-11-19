61944166621a257a8712f802fb320a8099d4c5c2
Noosdoo <satoshimatsumoto74@gmail.com>
2025-11-19 13:40:16 +0900

    ｓ

--- matsumoto/test_matsumoto_namae.py
+++ matsumoto/test_matsumoto_namae.py
@@ -546,7 +546,7 @@ def load_people_list(people_list_path=PEOPLE_LIST_FILE):
 # Step2: データセット構築（並列）
 # -----------------------
 def build_dataset_parallel(people_list_path=PEOPLE_LIST_FILE, dataset_path=DATASET_FILE,
-                           limit=None, max_workers=30, sleep=0.1): #max_workersを小さくすればエラーを防げる
+                           limit=None, max_workers=30, sleep=0.1): #max_workers（並列数）を小さくすればエラーを防げる
     
     # 人物リスト読み込み
     people = load_people_list(people_list_path)
